06a4e05848f683c2c71acd72facfeed399da53d3
tyczj <tyczj359@gmail.com>
2020-11-19 21:19:42 -0500

    Added missing files

---
+++ sharedmodule/build.gradle.kts
@@ -0,0 +1,106 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("kotlin-android-extensions")
+    id("kotlinx-serialization")
+}
+
+repositories {
+    gradlePluginPortal()
+    google()
+    jcenter()
+    mavenCentral()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
+kotlin {
+    android()
+    ios {
+        binaries {
+            framework {
+                baseName = "sharedmodule"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting{
+            dependencies {
+                implementation("io.ktor:ktor-client-core:1.4.2")
+                implementation("io.ktor:ktor-client-json:1.4.2")
+                implementation("io.ktor:ktor-client-serialization:1.4.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.core:core-ktx:1.3.2")
+                implementation("io.ktor:ktor-client-android:1.4.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.12")
+            }
+        }
+        val iosMain by getting{
+            dependencies{
+                implementation("io.ktor:ktor-client-ios:1.4.2")
+//                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:1.4.1")
+            }
+        }
+        val iosTest by getting
+    }
+}
+android {
+    compileSdkVersion(30)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(24)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    packagingOptions{
+        excludes.add("META-INF/*.kotlin_module")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+}
+val packForXcode by tasks.creating(Sync::class) {
+    group = "build"
+    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
+    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
+    val framework =
+        kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
+    inputs.property("mode", mode)
+    dependsOn(framework.linkTask)
+    val targetDir = File(buildDir, "xcode-frameworks")
+    from({ framework.outputDirectory })
+    into(targetDir)
+}
+tasks.getByName("build").dependsOn(packForXcode)
